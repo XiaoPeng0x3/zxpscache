cd300992c683d01c857ab5d65283baa58e7f0992
XiaoPeng <zxpcs0x3@gmail.com>
2025-05-18 20:29:58 +0800

    feat:实现分布式结点接口

---
+++ peers.go
@@ -0,0 +1,16 @@
+package mygroupcache
+
+import (
+	pb "my_groupcache/cachepb"
+)
+
+// PeerPicker is the interface that must be implemented to locate
+// the peer that owns a specific key.
+type PeerPicker interface {
+	PickPeer(key string) (peer PeerGetter, ok bool)
+}
+
+// PeerGetter is the interface that must be implemented by a peer.
+type PeerGetter interface {
+	Get(in *pb.Request, out *pb.Response) error
+}
